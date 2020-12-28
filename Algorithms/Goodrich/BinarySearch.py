aa6a86a0a76712cfadf2d2ce92e7dfdfa09e9141
Jans Pavlovs <jans.pavlovs@gmail.com>
2020-12-28 02:03:58 +0200

    Add binary search

---
+++ Algorithms/Goodrich/BinarySearch.py
@@ -0,0 +1,25 @@
+import unittest
+
+
+def binary_search(data, target, low, high) -> int:
+    if low > high:
+        return None
+    else:
+        mid = (low + high) // 2
+
+        if target == data[mid]:
+            return mid
+        elif target < data[mid]:
+            return binary_search(data, target, low, mid - 1)
+        else:
+            return binary_search(data, target, mid + 1, high)
+
+
+class TestBinarySearch(unittest.TestCase):
+    def test_one_item(self):
+        sut = binary_search([0], 0, 0, 1)
+        self.assertEqual(sut, 0)
+
+    def test_large_list(self):
+        sut = binary_search([1, 2, 3, 4, 5, 6, 7, 18, 19, 100], 18, 0, 9)
+        self.assertEqual(sut, 7)
